d282bd47c90267c147a2389965e9627006fc695a
francovaro <francovaro@gmail.com>
2023-12-18 17:41:49 +0100

    First commit

---
+++ CM7/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,37 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'example-standalone-inferencing-stm32h747i-disco' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:2.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube HAL:Common:1.11.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.11.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:GPIO:1.11.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:HSEM:1.11.0 */
+#define RTE_DEVICE_HAL_HSEM
+/* Keil::Device:STM32Cube HAL:PWR:1.11.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.11.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:Startup:1.11.0 */
+#define RTE_DEVICE_STARTUP_STM32H7XX    /* Device Startup for STM32H7 */
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ CM7/ei_main.h
@@ -0,0 +1,6 @@
+#ifndef _EI_MAIN_H_
+#define _EI_MAIN_H_
+
+extern "C" int ei_main(void);
+
+#endif
